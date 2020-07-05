96875138c0e0bcc6b2aae6edf53ef46e74d7edf3
Organic-Punch <sinho667@naver.com>
2020-07-05 18:10:08 +0900

    주사위네개

--- 백준/2484.cpp
+++ 백준/2484.cpp
@@ -9,15 +9,21 @@ int main() {
 		int arr[7] = { 0, }; int x, y, z, a;
 		cin >> x >> y >> z >> a;
 		arr[x]++; arr[y]++; arr[z]++; arr[a]++;
-		if (x != y && y != z && z != x) {
-			Max = max(Max, (max(x, max(y, max(z,a))) * 100));
-			continue;
+		if (x != y && x != z && x != a) {
+			if (y != z && y != a) {
+				if (z != a) {
+					Max = max(Max, (max(x, max(y, max(z, a))) * 100));
+					continue;
+				}
+			}
 		}
+		int temp = 0;
 		for (int i = 1; i < 7; i++) {
-			if (arr[i] == 4) Max = max(Max, (50000 + i * 5000));
-			else if (arr[i] == 3) Max = max(Max, (10000 + i * 1000));
-			else if (arr[i] == 2) Max = max(Max, (1000 + i * 100));
+			if (arr[i] == 4) { max(Max, (50000 + i * 5000)); break; }
+			else if (arr[i] == 3) temp += (10000 + i * 1000);
+			else if (arr[i] == 2) temp += (1000 + i * 100);
 		}
+		Max = max(Max, temp);
 	}
 	cout << Max;
 	return 0;
