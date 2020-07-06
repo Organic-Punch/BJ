72bd1c28a4d252f8cac918e8d379487c0dde2bb1
Organic-Punch <sinho667@naver.com>
2020-07-06 15:59:36 +0900

    미로탐색

---
+++ 백준/2178.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define pii pair<int,int>
+int arr[101][101], dir[4][2] = { {-1,0}, {1,0}, {0,-1}, {0,1} };
+bool vis[101][101];
+int main() {
+	int N, M; scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) scanf("%1d", &arr[i][j]);
+	queue<pii>q; q.push({ 0, 0 });
+	while (!q.empty() && arr[N-1][M-1] == 1) {
+		int y = q.front().first, x = q.front().second;
+		q.pop();
+		if (vis[y][x] == true) continue;
+		vis[y][x] = true;
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dir[i][0], nx = x + dir[i][1];
+			if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+			if (vis[ny][nx] || arr[ny][nx] == 0) continue;
+			q.push({ ny,nx });
+			arr[ny][nx] = arr[y][x] + 1;
+		}
+	}
+	printf("%d",arr[N - 1][M - 1]);
+	return 0;
+}
