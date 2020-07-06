9b9f0206c9ec59a1d04c51492f7cfc2c020e6e0b
Organic-Punch <sinho667@naver.com>
2020-07-06 17:54:32 +0900

    주사위게임-수정

--- 백준/2476.cpp
+++ 백준/2476.cpp
@@ -3,10 +3,9 @@
 using namespace std;
 
 int main() {
-	int N; cin >> N;
-	int Max = 0;
+	int N, Max = 0, x, y, z; cin >> N;
 	for (int i = 0; i < N; i++) {
-		int arr[7] = {0,}; int x, y, z;
+		int arr[7] = {0,};
 		cin >> x >> y >> z;
 		arr[x]++; arr[y]++; arr[z]++;
 		if (x!=y&&y!=z&&z!=x) {
@@ -14,12 +13,8 @@ int main() {
 			continue;
 		}
 		for (int i = 1; i < 7; i++) {
-			if (arr[i] == 3) {
-				Max = max(Max, (10000 + i * 1000));
-			}
-			else if (arr[i] == 2) {
-				Max = max(Max, (1000 + i * 100));
-			}
+			if (arr[i] == 3) Max = max(Max, (10000 + i * 1000));
+			else if (arr[i] == 2) Max = max(Max, (1000 + i * 100));
 		}
 	}
 	cout << Max;
