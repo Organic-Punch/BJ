f7e0ec6abd90cc4989495694d164a2e0e596347f
Organic-Punch <sinho667@naver.com>
2020-07-12 16:14:33 +0900

    연결요소의 개수

---
+++ 백준/11724.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int N, M;
+vector<int>arr[1001];
+vector<bool>visited;
+
+void DFS(int idx) {
+	visited[idx] = true;
+	for (int i = 0; i < arr[idx].size(); i++) {
+		if (!visited[arr[idx][i]]) {
+			DFS(arr[idx][i]);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> N >> M;
+	visited.resize(N + 1);
+	int cnt = 0;
+	for (int i = 0; i < M; i++) {
+		int a, b; cin >> a >> b;
+		arr[b].push_back(a);
+		arr[a].push_back(b);
+	}
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			DFS(i);
+			cnt++;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
