5e4dd4733e70ceca122befb2f4dbe555e5dabe79
Organic-Punch <sinho667@naver.com>
2020-07-26 16:34:40 +0900

    차이를 최대로

--- 백준/1937.cpp
+++ 백준/1937.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N, dir[4][2] = { {-1,0}, {1,0}, {0,-1}, {0,1} };
-vector<vector<int>>arr;
+vector<vector<int>>idx;
 vector<vector<bool>>vis;
 int DFS(int y, int x) {
 	int result = 1;
@@ -12,7 +12,7 @@ int DFS(int y, int x) {
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dir[i][0], nx = x + dir[i][1];
 		if (ny < 0 || nx < 0 || ny >= N || nx >= N || vis[ny][nx] == true) continue;
-		if (arr[y][x] < arr[ny][nx]) {
+		if (idx[y][x] < idx[ny][nx]) {
 			temp += DFS(ny, nx);
 			result = max(temp, result);
 		}
@@ -22,8 +22,8 @@ int DFS(int y, int x) {
 
 int main() {
 	cin >> N;
-	arr.resize(N, vector<int>(N,0));
-	for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) cin >> arr[i][j];
+	idx.resize(N, vector<int>(N,0));
+	for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) cin >> idx[i][j];
 	int cnt = 0;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
